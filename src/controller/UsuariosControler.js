290cd0fcd87c56389fd80db29a4ee905473a72d8
unknown <ramonsantos6053@digitalcollege.internal>
2025-06-12 14:23:45 -0300

    pull aceita

--- src/controller/UsuariosControler.js
+++ src/controller/UsuariosControler.js
@@ -1,4 +1,4 @@
-const { prisma } = require("../services");
+    const { prisma } = require("../services");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const saltRounds = 10;
